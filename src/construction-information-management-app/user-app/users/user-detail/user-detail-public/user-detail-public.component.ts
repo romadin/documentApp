bb884f6fc6a87fb005aebf4abe93771e0f967535
romadin <romario_dindajal@hotmail.com>
2019-04-15 00:19:46 +0200

    user detail public view

--- src/construction-information-management-app/user-app/users/user-detail/user-detail-public/user-detail-public.component.ts
+++ src/construction-information-management-app/user-app/users/user-detail/user-detail-public/user-detail-public.component.ts
@@ -13,12 +13,10 @@ export class UserDetailPublicComponent implements OnInit {
     @Input() imageSrc: string;
     @Output() closeView: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    constructor() {
-        this.projects = this.projects.filter(project => this.user.projectsId.find(projectId => projectId === project.getId()));
-        console.log(this.projects);
-    }
+    constructor() {}
 
     ngOnInit() {
+        this.projects = this.projects.filter(project => this.user.projectsId.find(projectId => projectId === project.getId()));
     }
 
     onCloseDetailView(e: Event): void {
