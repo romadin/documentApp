81c7756e0c66a35c94f2bc36a87ccf0ab466450c
romadin <romario_dindajal@hotmail.com>
2019-05-02 11:50:06 +0200

    animation template list to template detail

--- src/construction-information-management-app/templates-app/templates-overview/templates-overview.component.ts
+++ src/construction-information-management-app/templates-app/templates-overview/templates-overview.component.ts
@@ -5,11 +5,33 @@ import { Organisation } from '../../../shared/packages/organisation-package/orga
 import { TemplateService } from '../../../shared/packages/template-package/template.service';
 import { Template } from '../../../shared/packages/template-package/template.model';
 import { TemplateItemInterface } from '../../../shared/packages/template-package/interface/template-api-response.interface';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
-  selector: 'cim-templates-overview',
-  templateUrl: './templates-overview.component.html',
-  styleUrls: ['./templates-overview.component.css']
+    selector: 'cim-templates-overview',
+    templateUrl: './templates-overview.component.html',
+    styleUrls: ['./templates-overview.component.css'],
+    animations: [
+        trigger('slideLeftAndRight', [
+            transition('void => *', [
+                style({ width: '0'}),
+                animate('700ms cubic-bezier(0.0, 0.0, 0.2, 1)', style({ width: '100%'})),
+            ]),
+            transition('* => void', [
+                animate('700ms cubic-bezier(0.0, 0.0, 0.2, 1)', keyframes([
+                    style({ width: '0%'})
+                ])),
+            ])
+        ]),
+
+        trigger('slideRight', [
+            transition('* => void', [
+                animate('700ms cubic-bezier(0.0, 0.0, 0.2, 1)', keyframes([
+                    style({ transform: 'translateX(0)', offset: 1 }),
+                ])),
+            ])
+        ]),
+    ]
 })
 export class TemplatesOverviewComponent implements OnInit {
     templates: Template[];
