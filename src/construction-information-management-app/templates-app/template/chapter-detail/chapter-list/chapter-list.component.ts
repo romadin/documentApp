063fa0d4e39a446c93f88f7effd2d6cf94620dcc
romadin <romario_dindajal@hotmail.com>
2019-05-20 17:17:50 +0200

    fade in animation for list

--- src/construction-information-management-app/templates-app/template/chapter-detail/chapter-list/chapter-list.component.ts
+++ src/construction-information-management-app/templates-app/template/chapter-detail/chapter-list/chapter-list.component.ts
@@ -8,11 +8,25 @@ import { WorkFunction } from '../../../../../shared/packages/work-function-packa
 import {
     WorkFunctionUpdateBody
 } from '../../../../../shared/packages/work-function-package/interface/work-function-api-response.interface';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
-  selector: 'cim-chapter-list',
-  templateUrl: './chapter-list.component.html',
-  styleUrls: ['./chapter-list.component.css']
+    selector: 'cim-chapter-list',
+    templateUrl: './chapter-list.component.html',
+    styleUrls: ['./chapter-list.component.css'],
+    animations: [
+        trigger('toggleInView', [
+            transition('void => *', [
+                style({ opacity: '0'}),
+                animate('300ms cubic-bezier(0.0, 0.0, 0.2, 1)', style({ opacity: '1'})),
+            ]),
+            transition('* => void', [
+                animate('300ms cubic-bezier(0.0, 0.0, 0.2, 1)', keyframes([
+                    style({ opacity: '0'})
+                ])),
+            ])
+        ])
+    ]
 })
 export class ChapterListComponent implements OnInit {
     @Input() parent: WorkFunction;
